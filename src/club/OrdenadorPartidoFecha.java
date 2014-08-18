e7aa414989961831f88622b9b201bf408bb09809
Lucas <meli_lucas_1104@hotmail.com>
2014-08-18 19:13:02 -0300

    me pedia un par de metodos asique hice los override

--- src/club/OrdenadorPartidoFecha.java
+++ src/club/OrdenadorPartidoFecha.java
@@ -1,6 +1,10 @@
 package club;
 
 import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 
 public class OrdenadorPartidoFecha implements Comparator<Partido>{
 
@@ -14,6 +18,57 @@ public class OrdenadorPartidoFecha implements Comparator<Partido>{
 			return -1;
 		}
 		return 0;
+	}
+
+
+	
+
+	@Override
+	public Comparator<Partido> reversed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Partido> thenComparing(Comparator<? super Partido> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <U extends Comparable<? super U>> Comparator<Partido> thenComparing(
+			Function<? super Partido, ? extends U> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <U> Comparator<Partido> thenComparing(
+			Function<? super Partido, ? extends U> arg0,
+			Comparator<? super U> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Partido> thenComparingDouble(
+			ToDoubleFunction<? super Partido> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Partido> thenComparingInt(
+			ToIntFunction<? super Partido> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Partido> thenComparingLong(
+			ToLongFunction<? super Partido> arg0) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/club/Promedio.java
+++ src/club/Promedio.java
@@ -1,6 +1,11 @@
 package club;
 
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 
 public class Promedio extends CriterioOrden{
 	
@@ -67,6 +72,56 @@ public class Promedio extends CriterioOrden{
 		}
 		
 		return prom1.compareTo(prom2);
+	}
+
+	
+
+	@Override
+	public Comparator<Jugador> reversed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Jugador> thenComparing(Comparator<? super Jugador> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <U extends Comparable<? super U>> Comparator<Jugador> thenComparing(
+			Function<? super Jugador, ? extends U> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <U> Comparator<Jugador> thenComparing(
+			Function<? super Jugador, ? extends U> arg0,
+			Comparator<? super U> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Jugador> thenComparingDouble(
+			ToDoubleFunction<? super Jugador> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Jugador> thenComparingInt(
+			ToIntFunction<? super Jugador> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparator<Jugador> thenComparingLong(
+			ToLongFunction<? super Jugador> arg0) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 
